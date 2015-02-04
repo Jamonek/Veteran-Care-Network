d37c01bd91325402e39b0ebd79d9ce9c54f11d51
Jamone Kelly <xg.monie089@gmail.com>
2015-02-04 17:55:00 -0500

    Initial Commit

---
+++ VCN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VCN
+//
+//  Created by Jamone Alexander Kelly on 2/4/15.
+//  Copyright (c) 2015 Veteran Care Network, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ VCNTests/VCNTests.swift
@@ -0,0 +1,36 @@
+//
+//  VCNTests.swift
+//  VCNTests
+//
+//  Created by Jamone Alexander Kelly on 2/4/15.
+//  Copyright (c) 2015 Veteran Care Network, LLC. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class VCNTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
